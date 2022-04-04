adf10a5283a7e68cadee3491d6bae9bc494c2c50
PSeegers97 <pascal.seegers@web.de>
2022-04-04 15:52:35 +0200

    added Bla Bla Bla kat static poster with variable Fonts

---
+++ src/components/static-posters/variable-fonts/Bla-Bla-Bla-Kat/BlaBlaBlaKatStaticVF.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './BlaBlaBlaKatStaticVF.module.scss';
+
+function BlaBlaBlaKatStaticVF() {
+    return(
+        <div className={styles.BlaBlaBlaKatStaticVF}>
+            <div className={styles.firstLine}>BLA</div>
+            <div className={styles.secondLine}>BLA</div>
+            <div className={styles.thirdLine}>BLA</div>
+            <div className={styles.fourthLine}>KAT</div>
+        </div>
+    );
+}
+
+export default BlaBlaBlaKatStaticVF;
