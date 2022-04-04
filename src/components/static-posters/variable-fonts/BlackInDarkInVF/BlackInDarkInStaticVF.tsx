eeeac07dc356083f72a46cd51226d1f9fc4455ff
PSeegers97 <pascal.seegers@web.de>
2022-04-04 15:52:07 +0200

    added BlackInDarkIn static poster with variable Fonts

---
+++ src/components/static-posters/variable-fonts/BlackInDarkInVF/BlackInDarkInStaticVF.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from './BlackInDarkInStaticVF.module.scss';
+
+
+function BlackInDarkInStaticVF(){
+    return (
+        <div className={styles.BlackInDarkIn}>
+            <div className={styles.poster}>
+                <div className={styles.upperWrapper}>
+                    <div className={styles.blackLine}>
+                        Black
+                    </div>
+                    <div className={styles.inLine}>
+                        <div className={styles.firstIn}>
+                            in
+                        </div>
+
+                        <div className={`${styles.generalInformation} ${styles.smallBlock}`}>
+                            <div className={styles.dateLine}>3.12.2010 - 27.2.2011</div>
+                            <div className={styles.exhibitionName}>Die Neue Sammlung</div>
+                            <div className={styles.museumName}>The International Design Museum</div>
+                            <div className={styles.name}>Pinakothek der Moderne</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.lowerWrapper}>
+                    <div className={styles.darkLine}>
+
+                        Dark
+
+                    </div>
+                    <div className={`${styles.inLine} ${styles.authorLine}`}>
+                        <div className={`${styles.hubertusHamm} ${styles.smallBlock}`}>Hubertus Hamm</div>
+                        <div className={styles.secondIn} id={'secondIn'}>
+                            in
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default BlackInDarkInStaticVF;
