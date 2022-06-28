424b6a6d7628d822c052edbfec6e2d443387f856
PSeegers97 <p.seegers87@gmail.com>
2022-06-28 18:23:27 +0200

    added styling for Tsp poster back side

--- src/components/InteractivePosters/components/staticInteractive/TypographicSummerProgramBack/TypographicSummerProgramBack.tsx
+++ src/components/InteractivePosters/components/staticInteractive/TypographicSummerProgramBack/TypographicSummerProgramBack.tsx
@@ -17,20 +17,21 @@ function TypographicSummerProgramBack() {
     })
     return(
         <div className={styles.posterWrapper}>
-            <div className={styles.firstTextBlock}>
+            <div className={styles.upperTextBlock}>
+            <div className={`${styles.firstTextBlock}${styles.textBlock}`}>
                 Poster design is the prestigious discipline of reducing content
                 and layout ideas to transmit fast and simple messages. Working
                 with typography asks for the most simplified approaches.
                 Concepts are reduced to be represented only by the shapes
                 of the letters.
             </div>
-            <div className={styles.secondTextBlock}>
+            <div className={`${styles.secondTextBlock}${styles.textBlock}`}>
                 Here at www.typographic-summerprogram.ch our focus is on
                 analog typography and how to create conceptual poster designs.
                 We celebrate modern poster design in combination with new
                 explorations in letterpress printing.
             </div>
-            <div className={styles.thirdTextBlock}>
+            <div className={`${styles.thirdTextBlock}${styles.textBlock}`}>
                 We offer an intense 2 week summer program in Switzerland
                 for motivated students and professionals to learn new approaches
                 to Swiss typography. Swiss designer and letterpress printer
@@ -38,26 +39,28 @@ function TypographicSummerProgramBack() {
                 12 participants per session exploring analog experimental
                 typography in every detail.
             </div>
-            <div className={styles.fourthTextBlock}>
+            <div className={`${styles.fourthTextBlock}${styles.textBlock}`}>
                 Daily studio time, typographic layout exercises, introduction
                 to letterpress printing, excursions, talks, presentations, 20 tons
                 of letterpress equipment and much more! During the 2 weeks,
                 each student will develop a concept for a typographic poster that
                 will be letterpress printed towards the end of the workshop.
             </div>
+            </div>
             <div className={styles.generalInformation}>
                 <div className={styles.firstSession}>
-                    Session I
+                    <div className={styles.session}>Session I</div>
+                    <div className={styles.dateAndLocation}>June 17-30, 2018, Näfels, Switzerland</div>
                     June 17-30, 2018, Näfels, Switzerland
                 </div>
                 <div className={styles.secondSession}>
-                    Session II
-                    August 5-18, 2018, Näfels, Switzerland
+                    <div className={styles.session}>Session II</div>
+                    <div className={styles.dateAndLocation}>August 5-18, 2018, Näfels, Switzerland</div>
                 </div>
                 <div className={styles.registrationInformation}>
-                    Fee: CHF 1750.-
-                    Apply online now!
-                    (Application deadline for both sessions: March 30, 2018)
+                    <div className={styles.fee}>Fee: CHF 1750.-</div>
+                    <div className={styles.callToAction}>Apply online now!</div>
+                    <div className={styles.deadline}>(Application deadline for both sessions: March 30, 2018)</div>
                 </div>
                 <div className={styles.link}>typographic-summerprogram.ch</div>
             </div>
